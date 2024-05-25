b1844ea7ffac78bdf064d465e566a8e67b046ef0
r1FH <91796669+r1FH@users.noreply.github.com>
2024-05-25 20:42:50 +0200

    Create tapbot.lua

---
+++ tapbot.lua
@@ -0,0 +1,29 @@
+--[[
+▄▄▄█████▓ ▄▄▄       ██▓███   ▄▄▄▄    ▒█████  ▄▄▄█████▓
+▓  ██▒ ▓▒▒████▄    ▓██░  ██▒▓█████▄ ▒██▒  ██▒▓  ██▒ ▓▒
+▒ ▓██░ ▒░▒██  ▀█▄  ▓██░ ██▓▒▒██▒ ▄██▒██░  ██▒▒ ▓██░ ▒░
+░ ▓██▓ ░ ░██▄▄▄▄██ ▒██▄█▓▒ ▒▒██░█▀  ▒██   ██░░ ▓██▓ ░ 
+  ▒██▒ ░  ▓█   ▓██▒▒██▒ ░  ░░▓█  ▀█▓░ ████▓▒░  ▒██▒ ░ 
+  ▒ ░░    ▒▒   ▓▒█░▒▓▒░ ░  ░░▒▓███▀▒░ ▒░▒░▒░   ▒ ░░   
+    ░      ▒   ▒▒ ░░▒ ░     ▒░▒   ░   ░ ▒ ▒░     ░    
+  ░        ░   ▒   ░░        ░    ░ ░ ░ ░ ▒    ░      
+               ░  ░          ░          ░ ░           
+                                  ░                   
+
+tapbot.cc runs you || made by bolt and ruque
+
+<3 skidded garbage:
+
+blazed.lol
+bladlock
+imperial
+blazing
+aura.cc
+aura x or whatever it was called
+anything made by vio or xenaki
+anything made by lion
+anything made by bladlock devs
+anything made by nxt.lol
+]]--
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/r1FH/sprites/main/Protected_4649091927754263.lua.txt"))()
